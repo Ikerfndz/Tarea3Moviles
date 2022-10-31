3f88c84c4f6fe4bba8878a274b85d71cad64aaff
Ikerfndz <jeu39212@educastur.es>
2022-10-31 20:14:33 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tarea3moviles/MainActivityLinear.java
@@ -0,0 +1,14 @@
+package com.example.tarea3moviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityLinear extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear);
+    }
+}
---
+++ app/src/main/java/com/example/tarea3moviles/MainActivityRelative.java
@@ -0,0 +1,14 @@
+package com.example.tarea3moviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityRelative extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative);
+    }
+}
